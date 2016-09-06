1a38401080444323ae4c8468e02f15aed2865178
Jonathan Dushoff <dushoff@mcmaster.ca>
2016-09-06 14:11:25 -0400

    Get rid of RHS code. Make (old) funhist() into a wrapper function.

--- funhist.R
+++ funhist.R
@@ -4,12 +4,6 @@ funZones = function()
 	layout(zones, widths=c(1/5,4/5), heights=c(1/5,4/5))
 }
 
-rightZones = function()
-{
-	zones=matrix(c(2,0,1,3), ncol=2, byrow=TRUE)
-	layout(zones, widths=c(4/5,1/5), heights=c(1/5,4/5))
-}
-
 funHist = function(x, f, parms=NULL , finv=NULL, means=TRUE, top=NULL)
 {
 	y <- f(x, parms)
@@ -43,40 +37,11 @@ funHist = function(x, f, parms=NULL , finv=NULL, means=TRUE, top=NULL)
 	)
 }
 
-### Deprecated version of funHist; still used in talk-panel figures. Fix and eliminate (have them all call funZones once instead)
-### Need to figure out why I had that weird par() statement.
+### Old version; nothing wrong with it I guess
 funhist = function(x, f, parms=NULL , finv=NULL, means=TRUE, top=NULL)
 {
-	zones=matrix(c(0,2,3,1), ncol=2, byrow=TRUE)
-	layout(zones, widths=c(1/5,4/5), heights=c(1/5,4/5))
-
-	y <- f(x, parms)
-
-	xhist = hist(x, plot=FALSE)
-	yhist = hist(y, plot=FALSE)
-
-	top = max(c(xhist$counts, yhist$counts))
-
-	xs <- seq(min(x), max(x), length.out=101)
-	ys = f(xs, parms)
-
-	par(mar=c(3,3,1,1))
-	plot(xs, ys , type="l", yaxt="n")
-
-	abline(v=mean(x), lwd=2)
-
-	if(means){
-		abline(v= finv(mean(y), parms), col="red", lwd=2)
-		abline(h=mean(y), col="red", lwd=2)
-	}
-
-	par(mar=c(0,3,1,1))
-	barplot(xhist$counts, axes=FALSE, ylim=c(0, top), space=0)
-
-	par(mar=c(3,0,1,1))
-	barplot(yhist$counts,
-		axes=FALSE, xlim=c(top, 0), space=0, horiz=TRUE)
-	par(oma=c(3,3,0,0))
+	funZones()
+	funHist(x, f, parms, finv, means, top)
 }
 
 expfun <- function(x, parms){return(with(parms, exp(-x/Tc)))}
