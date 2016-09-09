0351b753ae765eb8b4f4e10c01c3fb2410eecbde
Jonathan Dushoff <dushoff@mcmaster.ca>
2016-09-09 10:40:25 -0400

    Autocommit (Generation_distributions) On branch master Your branch is up-to-date with 'origin/master'.
    Changes to be committed:
      (use "git reset HEAD <file>..." to unstage)
    
    	modified:   Makefile
    	modified:   paper.R
    
    Untracked files:
      (use "git add <file>..." to include in what will be committed)
    
    	commit.time
    	localdir/
    	mk.mk
    	steps.wide.png
    	ww.mk

--- paper.R
+++ paper.R
@@ -3,8 +3,4 @@ dgen <- dgen[dgen<=40]
 leftZones()
 funHist(dgen, expfun, finv=logfun, parms=list(Tc=15), top=2000)
 funHist(dgen, expfun, finv=logfun, parms=list(Tc=45), top=2000)
-<<<<<<< HEAD
-=======
 # funHist(dgen, idfun, finv=idfun, parms=list(Tc=450), top=2000)
-
->>>>>>> Autocommit (Generation_distributions)
