38b0d8d09b63e9c57143137ec54f24cc1525130a
Jonathan Dushoff <dushoff@mcmaster.ca>
2016-12-02 14:28:06 -0500

    Autocommit (Generation_distributions) On branch master Your branch is up-to-date with 'origin/master'. Changes to be committed:   (use "git reset HEAD <file>..." to unstage)
    	modified:   XN.R
    	modified:   gx.R
    
    Untracked files:
      (use "git add <file>..." to include in what will be committed)
    
    	.localdir/
    	commit.time
    	localdir/
    	mk.mk
    	steps.wide.png
    	ww.mk

--- XN.R
+++ XN.R
@@ -1,11 +1,11 @@
-genX <- function(rho, kappa=0){
+## Probably no point to having the unvectorized functions
+genX <- Vectorize(function(rho, kappa=0){
 	if (kappa==0)
 		return(exp(rho))
 	return((1+rho*kappa)^(1/kappa))
-}
+})
 
-vgenX <- Vectorize(genX)
-
-genN <- function(rho, shape=NULL, kappa=NULL){
-	return(rho/(1-1/genX(rho, shape, kappa)))
-}
+genN <- Vectorize(function(rho, kappa=NULL){
+	if (rho==0) return(1)
+	return(rho/(1-1/genX(rho, kappa)))
+})
--- gx.R
+++ gx.R
@@ -2,7 +2,30 @@ library(ggplot2); theme_set(theme_bw())
 library(tidyr)
 
 rho <- seq(0, 3, length.out=101)
-kappa <- c(1, 1/2, 1/4, 0)
+kappa <- c(0, 1/4, 1/2, 1)
 
-outer(rho, kappa, function(x, y){return(vgenX(x, kappa=y))})
 
+framePlot <- function(m){
+	f <- (data.frame(rho, m=m)
+		%>% gather(kappa, R, m.1:m.4)
+	)
+	knames <- unique(f$kappa)
+	print(f)
+
+	print(
+		ggplot(f, aes(x=rho, y=R, linetype=kappa))
+		+ geom_line()
+		+ xlab("Interval")
+		+ ylab("Effect")
+		+ ylim(c(1, 10))
+		+ scale_linetype_discrete(
+			breaks=knames, labels=as.numeric(kappa)
+		)
+	)
+}
+
+gx <- outer(rho, kappa, function(x, y){return(genX(x, kappa=y))})
+gn <- outer(rho, kappa, function(x, y){return(genN(x, kappa=y))})
+
+framePlot(gx)
+framePlot(gn)
