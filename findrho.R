36e6f762222ed967727d59754743894853aff420
parksw3 <parksw3@mcmaster.ca>
2017-06-23 01:20:41 -0400

    update measles

--- findrho.R
+++ findrho.R
@@ -2,7 +2,11 @@ rfun <- function(x, Reff, gen, gbar){
     Reff - EulerR(gen, gbar/x)
 }
 
-rho_eff <- sapply(Reff, function(x){
-    res <- uniroot(rfun, c(0, 10), gen=gen, Reff=x, gbar=gbar)
-    return(res$root)
-})
+if (is.null(Reff)) {
+    rho_eff <- NULL
+} else {
+    rho_eff <- sapply(Reff, function(x){
+        res <- uniroot(rfun, c(0, 10), gen=gen, Reff=x, gbar=gbar)
+        return(res$root)
+    })
+}
