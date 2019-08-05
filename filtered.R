879920eac2e320e2c7955d4a2eaafa13245d8542
Jonathan Dushoff <dushoff@mcmaster.ca>
2019-08-06 06:07:04 +0900

    Better range for C?

--- filtered.R
+++ filtered.R
@@ -2,10 +2,11 @@ top <- 1600
 breaks <- 20
 dgen <- dgen[dgen<=40]
 
-funhist(dgen, expfun, finv=logfun, top=top, parms=list(Tc=15))
+funhist(dgen, expfun, finv=logfun, top=top, parms=list(Tc=10))
+funhist(dgen, expfun, finv=logfun, top=top, parms=list(Tc=20))
 funhist(dgen, expfun, finv=logfun, top=top, parms=list(Tc=30))
-funhist(dgen, expfun, finv=logfun, top=top, parms=list(Tc=45))
 
+## More mathematical explorations
 funhist(dgen, logfun, finv=expfun, parms=list(Tc=-1), means=FALSE)
 funhist(dgen, logfun, finv=expfun, parms=list(Tc=-1))
 funhist(dgen, recip, finv=recip)
